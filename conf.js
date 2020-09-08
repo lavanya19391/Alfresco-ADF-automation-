444b4df3ab3414a2d116e4b696ccf40ed4530c1d
lavanya19391 <59205882+lavanya19391@users.noreply.github.com>
2020-09-08 10:29:56 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,14 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['QA.js']
+  specs: ['QA.js'],
+  jasmineNodeOpts: {
+    // Jasmine default timeout
+    defaultTimeoutInterval: 60000,
+    },
+    capabilities: {
+      'browserName': 'chrome',
+      'chromeOptions': {
+        args: ['--disable-browser-side-navigation'] 
+      }   
+  }
 } 
